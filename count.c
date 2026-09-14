7dd7445d72aeb62b088aacaa91fb899bb380ab80
Feng <2950148400@qq.com>
2026-09-14 19:36:49 +0800

    reagain

--- count.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    printf("25+66=%d\n",25+66);
-    return 0;
-}
--- hello.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    printf("Hello World! L will win!");
-    return 0;
-}
