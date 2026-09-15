5cb880eaa891fd71e7ddc0204ba15c44da66d846
Feng <2950148400@qq.com>
2026-09-15 18:47:59 +0800

    height

--- height.c
+++ height.c
@@ -5,10 +5,10 @@ int main()
     printf("请分别输入身高的英尺和英寸，"
     "如输入\"5 7\"表述五英尺七英寸：");
 
-    int foot;
-    int inch;
+    double foot;
+    double inch;
 
-    scanf("%d %d",&foot,&inch);
+    scanf("%lf %lf",&foot,&inch);
     printf("身高是%f米。\n",((foot + inch/12)*0.3048));
 
     return 0;
--- text.c
+++ text.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    printf("%d\n",10/3*3);
+    printf("%f\n",10.0/3*3);
     
     return 0;
 
