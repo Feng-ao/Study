7121f4c50d8f932a27242e549fdfb002b74f978c
Feng <2950148400@qq.com>
2026-09-15 10:17:14 +0800

    change

---
+++ change3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int amount=0;
+    int price=100;
+
+    printf("您共花费了（元）：");
+    scanf("%d",&price);
+
+    printf("您给出（元）：");
+    scanf("%d",&amount);
+
+    int change=amount-price;
+    printf("找您%d元",change);
+
+    return 0;
+}
